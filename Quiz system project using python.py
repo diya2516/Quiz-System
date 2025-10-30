98afbc9780ccbd0dd1b1c97a56374ddf955b2b59
DIYA THOMRE <165244797+diya2516@users.noreply.github.com>
2025-10-30 14:25:14 +0530

    Add files via upload

---
+++ Quiz system project using python.py
@@ -0,0 +1,88 @@
+questions = {
+    1: {
+        "tech":"Python",
+        "question":"What is Python?",
+        "options": {
+            "A":"general-purpose",
+            "B":"high-level",
+            "C":"Interpreted&Dynamically-typed",
+            "D":"All of the above"
+        },
+        "answer": "D"
+    },
+    2: {
+       "tech":"MySQL",
+       "question":"Which MySQL statement is used to select data from a databse?",
+       "options":{
+           "A":"SELECT",
+           "B":"EXTRACT",
+           "C":"GET",
+           "D":"OPEN"
+           },
+       "answer":"A"
+    },
+    3: {
+       "tech":"Python",
+       "question":"Which keyword is used for function in python?",
+       "options":{
+           "A":"fun",
+           "B":"define",
+           "C":"def",
+           "D":"function"
+           },
+       "answer":"def"
+    },
+}
+
+user_score=[]
+admin_id="Diya"
+admin_password="Diya@1234"
+q_no=4
+
+
+while True:
+    print(''*15,'QUIZ SYSTEM',''*15)
+    print("1. Admin Login")
+    print("2. User login")
+    print("3. Exit")
+    choice = input("Enter choice: ")
+    
+    if choice == 1:
+        id_input=input("Enter Admin id: ")
+        pass_input=input("Enter Password: ")
+        if id_input=admin_id and pass_input=admin_password:
+            while True:
+                print("\n--Admin Panel--")
+                print("1. Add Question")
+                print("2. Modify Question")
+                print("3. Delete Question")
+                print("4. View all questions")
+                print("5. View all User Details")
+                print("6. Logout")
+                admin
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+       
+       
+           
